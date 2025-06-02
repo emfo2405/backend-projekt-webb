65352b5ed6e8947752aa1a164ce9d75a83bb5bce
emfo2405 <emfo2405@student.miun.se>
2025-06-02 10:18:39 +0200

    Funktion för att logga in skapad

---
+++ src/js/login.js
@@ -0,0 +1,59 @@
+//Funktion för inloggning för en användare
+async function loginEmployee(event) {
+    event.preventDefault();
+
+    //Hämta användarnamn och lösenord från inloggningsformulär
+    let username = document.getElementById("login-username");
+    let password = document.getElementById("login-password");
+
+    let users = {
+        username: username.value,
+        password: password.value 
+    };
+
+    //Koppla till POST-anrop för inloggning
+    try {
+        //Koppla till API
+        let response = await fetch('http://localhost:3001/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(users)
+        });
+        
+        let data = await response.json();
+
+        //Om allt är ok sparas token till local storage och hemligt innehåll hämtas
+        if(response.ok) {
+            localStorage.setItem("token", data.response.token);
+
+            let newResponse = await fetch('http://localhost:3001/api/secret', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + data.response.token,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            //Om allt stämmer visas den skyddade sidan
+            if (newResponse.ok) {
+                window.location.href = "omoss.html";
+            }
+
+        } else {
+            alert("Inloggningen misslyckades, kontrollera användarnamn och lösenord")
+        }
+
+    } catch (err) {
+        console.error("Något gick fel vid inloggning");
+        alert("Något gick fel vid inloggning, försök igen")
+    }
+    
+}
+
+//Hämta inloggningsknapp från formulär
+let loginButton = document.getElementById("login-button");
+
+//Lägger till funktion på knappen
+loginButton.onclick = loginEmployee;
