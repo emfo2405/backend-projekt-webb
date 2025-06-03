900985ffa8e3d9633474fa28dfd88ca23d00687f
emfo2405 <emfo2405@student.miun.se>
2025-06-03 08:10:46 +0200

    Funktion för att publicera meny skapad

--- src/js/login.js
+++ src/js/login.js
@@ -18,16 +18,20 @@ if(token) {
         //Om token stämmer visas inte inloggningen utan innehållet på sidan
          if (newResponse.ok) {
             document.getElementById("secret-content").style.display = "block";
+            document.getElementById("anställd-content").style.display = "none";
+            postMenu();
             
             //Om token inte stämmer visas inloggning och token tas bort från localStorage
         } else {
             document.getElementById("secret-content").style.display = "none";
+            document.getElementById("anställd-content").style.display = "block";
         localStorage.removeItem("token");
             
         //Om token inte finns visas inloggning
     } }  else {
 
         document.getElementById("secret-content").style.display = "none";
+        document.getElementById("anställd-content").style.display = "block";
 }    
 }
 
@@ -73,6 +77,8 @@ async function loginEmployee(event) {
             //Om allt stämmer visas den skyddade sidan
             if (newResponse.ok) {
                 document.getElementById("secret-content").style.display = "block";
+                document.getElementById("anställd-content").style.display = "none";
+                postMenu();
             }
 
         } else {
@@ -91,3 +97,35 @@ let loginButton = document.getElementById("login-button");
 
 //Lägger till funktion på knappen
 loginButton.onclick = loginEmployee;
+
+//Funktion för att publicera menyn
+async function postMenu() {
+    //Hämtar in list-element från HTML
+    let menuList = document.getElementById("menu-list");
+
+    //Tömmer listan så det inte blir dubbletter
+    menuList.innerHTML="";
+
+    //Hämtar in information från API och databasen som ska publiceras
+    let response = await fetch('http://localhost:3001/api/menu', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    
+    let data = await response.json();
+    let info = data.rows;
+    console.log(data.rows);
+
+    //För varje rad i databasen ska ett li-element skapas i listan som visar informationen
+    info.forEach(input => {
+        //Skapar ett li-element
+        let li = document.createElement("li");
+        li.innerHTML = `<h3 id="post-h3"> ${input.drinkname}, ${input.price}</h3> <br> <p> Beskrivning: ${input.description} <br> ${input.allergens} </p>`
+
+        //Lägger till li-element 
+        menuList.appendChild(li);
+
+    });
+}
